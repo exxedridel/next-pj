a4eb91c606efd7d239df2f7692f2e00ded49933a
hevedrios <exxed.ridel@gmail.com>
2023-01-27 14:10:37 -0600

    ADD My own navbar setapps component to test Netlify build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 // import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -17,75 +17,87 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-light box-shadow">
+          <div className="container-fluid nav-cont">
             <a className="navbar-brand" href="#">
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
               Set<b>apps</b>
             </a>
             <button
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 bold">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Pricing
+                    HOME
                   </a>
                 </li>
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle"
                     href="#"
+                    id="navbarDropdown"
                     role="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
-                    Dropdown link
+                    SERVICES
                   </a>
-                  <ul className="dropdown-menu">
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
                     <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
+                      <Link href="#" className="dropdown-item">
+                        Webapp calculator
+                      </Link>
                     </li>
                     <li>
                       <a className="dropdown-item" href="#">
-                        Another action
+                        Logo-svg creation
                       </a>
                     </li>
                     <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
                       <a className="dropdown-item" href="#">
-                        Something else here
+                        Improve performance
                       </a>
                     </li>
                   </ul>
                 </li>
+                <li className="nav-item">
+                  <Link href="#" className="nav-link">
+                    ABOUT
+                  </Link>
+                </li>
               </ul>
-            </div>
               <a href="https://wa.me/message/QNWKISFC6MBJI1">
                 <button className="btn btn-primary nav-btn" type="submit">
                   LET&apos;S CONNECT
                 </button>
               </a>
+            </div>
           </div>
         </nav>
 
