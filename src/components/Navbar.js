eb1f5ff98bf6739053289e656f19cc958245e47a
hevedrios <exxed.ridel@gmail.com>
2023-01-27 16:44:26 -0600

    ADD full Setapps navbar migration

---
+++ src/components/Navbar.js
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light box-shadow">
+      <div className={`container-fluid ${styles.navCont}`}>
+        <a className={`navbar-brand ${styles.navbarBrand}`} href="#">
+          <Image
+            src="/setapps-logo.svg"
+            alt="Setapps Logo"
+            className={styles.logo}
+            width={26}
+            height={26}
+            priority
+          />
+          Set<b>apps</b>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 bold">
+            <li className="nav-item">
+              <a className={`nav-link active ${styles.navLink}`} aria-current="page" href="#">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className={`nav-link dropdown-toggle ${styles.navLink}`}
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                SERVICES
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link href="#" className="dropdown-item">
+                    Web app calculator
+                  </Link>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Logo-svg creation
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Improve performance
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link href="#" className={`nav-link ${styles.navLink}`}>
+                ABOUT
+              </Link>
+            </li>
+          </ul>
+          <a href="https://wa.me/message/QNWKISFC6MBJI1">
+            <button className={`btn btn-primary ${styles.navBtn}`} type="submit">
+              LET&apos;S CONNECT
+            </button>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 // import { Inter } from "@next/font/google";
+import Navbar from "../components/Navbar";
 import styles from "@/styles/Home.module.css";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -16,90 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
       <main className={styles.main}>
-        <nav className="navbar navbar-expand-lg bg-light box-shadow">
-          <div className="container-fluid nav-cont">
-            <a className="navbar-brand" href="#">
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-              Set<b>apps</b>
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 bold">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    HOME
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    SERVICES
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <Link href="#" className="dropdown-item">
-                        Webapp calculator
-                      </Link>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Logo-svg creation
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Improve performance
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <Link href="#" className="nav-link">
-                    ABOUT
-                  </Link>
-                </li>
-              </ul>
-              <a href="https://wa.me/message/QNWKISFC6MBJI1">
-                <button className="btn btn-primary nav-btn" type="submit">
-                  LET&apos;S CONNECT
-                </button>
-              </a>
-            </div>
-          </div>
-        </nav>
 
         {/* <div className={styles.description}>
           <p>
