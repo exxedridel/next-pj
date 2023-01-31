28e44ef0f8bb7bb329b6f4d49a7bee0e90176272
hevedrios <exxed.ridel@gmail.com>
2023-01-31 15:32:53 -0600

    ADD i18n with 2 languages

---
+++ public/locale/en.js
@@ -0,0 +1,4 @@
+export default {
+   title: "Hello world",
+   description: "This is an example of aproject using i18n",
+}
---
+++ public/locale/es.js
@@ -0,0 +1,4 @@
+export default {
+   title: "Hola mundo",
+   description: "Este es un ejemplo de proyecto usando i18n",
+}
