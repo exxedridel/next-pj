9b34e5cb9354f2ed13a1db7eca73106b1d857254
hevedrios <exxed.ridel@gmail.com>
2023-01-31 13:57:43 -0600

    FIX Navbar styles with re-importing component in each page, ADD favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Script from "next/script";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="setapps icon" href="/favicon.png" />
+      </Head>
+
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import styles from "@/styles/About.module.css"
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import styles from "@/styles/About.module.css";
 
 const About = () => {
   return (
-    <main className={styles.main}>
-      <h1>About Us</h1>
-    </main>
-  )
-}
+    <>
+      <Head>
+        <title>Setapps | About</title>
+        <meta name="description" content="Created by Setapps" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Navbar />
+      <main>
+        <h1>About Us</h1>
+      </main>
+    </>
+  );
+};
 
-export default About
+export default About;
