cfa2e403d60d496d5a0eff4264f58c6db15e0448
hevedrios <exxed.ridel@gmail.com>
2023-02-01 13:25:12 -0600

    FIX change favicon pack to version 0.2 to display in safari mobile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,12 +7,12 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#F5F5F5"/>
-        <meta name="msapplication-TileColor" content="#f5f5f5"/>
-        <meta name="theme-color" content="#f5f5f5"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#383838" />
+        <meta name="msapplication-TileColor" content="#f5f5f5" />
+        <meta name="theme-color" content="#f5f5f5" />
       </Head>
 
       <Script
