f3e9484a51acf40a06d7a78f707b8733ef715a99
hevedrios <exxed.ridel@gmail.com>
2023-02-07 23:20:13 -0600

    HOTFIX punctuacion syntax

--- src/pages/services/webapp-calculator.jsx
+++ src/pages/services/webapp-calculator.jsx
@@ -1,21 +1,21 @@
 import { useContext } from "react";
 import AppContext from "@/context/AppContext";
-import Head from "next/head";
+// import Head from "next/head";
 import styles from "./webapp-calculator.module.css";
 
-const webappCalculator = () => {
+const WebappCalculator = () => {
   const { t } = useContext(AppContext);
   return (
     <main className={`${styles.main} container flow`}>
       <section className={styles.title}>
         <h1 className="ff-sans-title fs-800">Web App Calculator</h1>
         <p className="fs-500">
-          With this Setapps' tool you can quote how much your desired web app
-          it's going to be.
+          With this Setapps&lsquo; tool you can quote how much your desired web app
+          it&apos;s going to be.
         </p>
       </section>
     </main>
   );
 };
 
-export default webappCalculator;
+export default WebappCalculator;
