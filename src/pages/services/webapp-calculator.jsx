f94bf82f586981bf91f18fc7ed131b7ec1fdaabe
hevedrios <exxed.ridel@gmail.com>
2023-02-09 21:34:08 -0600

    ADD the rest of styles for webapp calculator component and results

--- src/pages/services/webapp-calculator.jsx
+++ src/pages/services/webapp-calculator.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AppContext from "@/context/AppContext";
 import Head from "next/head";
 import HeroCalculator from "@/components/HeroCalculator";
@@ -6,6 +6,35 @@ import styles from "@/styles/Webapp-calculator.module.css";
 
 const WebappCalculator = () => {
   const { t } = useContext(AppContext);
+  const [formData, setFormDData] = useState({
+    webType: "",
+    design: "",
+    logo: "",
+    navButtons: "",
+    animations: "",
+    languages: "",
+    modes: "",
+    domainHost: "",
+    analytics: "",
+  });
+  const [quote, setQuote] = useState(0);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormDData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(formData);
+  }
+
   return (
     <>
       <Head>
@@ -21,92 +50,181 @@ const WebappCalculator = () => {
           <h2 className={`${styles.title} ff-sans-title fs-600`}>
             Select the features your web app needs:
           </h2>
-          <ul className={`${styles.features} flow-normal`}>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-            <li className={`${styles.feature} fs-300`}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-              optio!
-              <br />
-              <select name="" id="">
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo Plateu Azcapotzalco</option>
-                <option value="violet">Violet</option>
-              </select>
-            </li>
-          </ul>
+          <form onSubmit={handleSubmit}>
+            <ol className={`${styles.features} `}>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="static-website">Static web site</option>
+                  <option value="blog-like">Blog like</option>
+                  <option value="e-commerce">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="design">Do you need design services?</label>
+                <br />
+                <select
+                  id="design"
+                  name="design"
+                  onChange={handleChange}
+                  value={formData.design}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="designer-service">
+                    Yes, need my own design
+                  </option>
+                  <option value="from-sketch">No, Ive a sketch</option>
+                  <option value="from-design">No, already got it</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="logo">
+                  Do you need logo creation services?
+                </label>
+                <br />
+                <select
+                  id="logo"
+                  name="logo"
+                  onChange={handleChange}
+                  value={formData.logo}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="designer-service">
+                    Yes, need my own logo
+                  </option>
+                  <option value="from-pnglogo">No, Ive a png logo</option>
+                  <option value="from-svglogo">No, Ive a svg logo</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+              <li className={`${styles.feature} fs-300`}>
+                <label htmlFor="webType">
+                  What kind of Web App do you need?
+                </label>
+                <br />
+                <select
+                  id="webType"
+                  name="webType"
+                  onChange={handleChange}
+                  value={formData.webType}
+                >
+                  <option value="none"> - - - - - - - - </option>
+                  <option value="landing-page">Landing page</option>
+                  <option value="yellow">Static web site</option>
+                  <option value="green">Blog like</option>
+                  <option value="green">E-commerce</option>
+                </select>
+              </li>
+            </ol>
+            <div  className={styles.result}>
+              <h2 className="ff-sans-title fs-600">The approximate cost is:</h2>
+              <p className={`${styles.price} fs-800`}><span className="fs-500">$</span>{quote}<span className="fs-500">.00</span></p>
+              <button
+                className={`btn btn-primary uppercase ff-sans-title ${styles.submitBtn}`}
+              >
+                Checkout
+              </button>
+            </div>
+          </form>
         </section>
       </main>
     </>
