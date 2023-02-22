9bfaec3309aaf97a85ed31ec31b05c9216dbe39d
hevedrios <exxed.ridel@gmail.com>
2023-02-22 17:54:05 -0600

    FIX footer right content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={`${styles.footer}`}>
-      <p className="container text-center fs-200">© Copyright Setapps™ 2023. Todos los derechos reservados.</p>
+      <p className="container text-center fs-200">© Setapps™ 2023 - Todos los derechos reservados.</p>
     </footer>
   );
 }
