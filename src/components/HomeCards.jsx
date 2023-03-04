56de442a148cf2c256b224587989c8f404d1f2a5
hevedrios <exxed.ridel@gmail.com>
2023-03-03 20:51:49 -0600

    ADD HomeCards component images and icons

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -8,84 +8,81 @@ const HomeCards = () => {
   const { t } = useContext(AppContext);
   return (
     <section className={styles.homeCards}>
-      
-        <Link className={styles.card} href="/services/webapp-calculator">
-          <Image
-            className={styles.image}
-            src="/night-stars-bg.jpg"
-            alt="contact card"
-            height={220}
-            width={300}
-            priority
-          />
-          <div className={styles.iconDiv}>
+      <Link className={styles.card} href="/services/webapp-calculator">
+        <Image
+          className={styles.image}
+          src="/nextjs-framework.png"
+          alt="next.js framework"
+          height={392}
+          width={855}
+          priority
+        />
+        <div className={styles.iconDiv}>
+          <div className={styles.bgIcon}>
             <Image
-              src="/setapps.webp"
-              alt="Setapps icon"
-              height={68}
-              width={68}
+              src="/technology.png"
+              alt="technology icon"
+              height={64}
+              width={64}
               priority
             />
           </div>
-          <div className={`${styles.info}`}>
-            <h3>{t.home.advantage.title1}</h3>
-            <p>
-            {t.home.advantage.description1}
-            </p>
-          </div>
-        </Link>
-        <Link className={styles.card} href="/products">
-          <Image
-            className={styles.image}
-            src="/night-stars-bg.jpg"
-            alt="contact card"
-            height={220}
-            width={300}
-            priority
-          />
-          <div className={styles.iconDiv}>
+        </div>
+        <div className={`${styles.info}`}>
+          <h3>{t.home.advantage.title1}</h3>
+          <p>{t.home.advantage.description1}</p>
+        </div>
+      </Link>
+      <Link className={styles.card} href="/products">
+        <Image
+          className={styles.image}
+          src="/satisfaction.jpg"
+          alt="happy entrepreneurs"
+          height={427}
+          width={640}
+          priority
+        />
+        <div className={styles.iconDiv}>
+          <div className={styles.bgIcon}>
             <Image
-              src="/setapps.webp"
-              alt="Setapps icon"
-              height={68}
-              width={68}
+              src="/satisfaction-icon.png"
+              alt="satisfaction icon"
+              height={64}
+              width={64}
               priority
             />
           </div>
-          <div className={`${styles.info}`}>
-            <h3>{t.home.advantage.title2}</h3>
-            <p>
-             {t.home.advantage.description2}
-            </p>
-          </div>
-        </Link>
-        <Link className={styles.card} href="/about">
-          <Image
-            className={styles.image}
-            src="/night-stars-bg.jpg"
-            alt="contact card"
-            height={220}
-            width={300}
-            priority
-          />
-          <div className={styles.iconDiv}>
+        </div>
+        <div className={`${styles.info}`}>
+          <h3>{t.home.advantage.title2}</h3>
+          <p>{t.home.advantage.description2}</p>
+        </div>
+      </Link>
+      <Link className={styles.card} href="/about">
+        <Image
+          className={styles.image}
+          src="/always-up.jpg"
+          alt="contact card"
+          height={427}
+          width={640}
+          priority
+        />
+        <div className={styles.iconDiv}>
+          <div className={styles.bgIcon}>
             <Image
-              src="/setapps.webp"
+              src="/maintenance-icon.png"
               alt="Setapps icon"
-              height={68}
-              width={68}
+              height={64}
+              width={64}
               priority
             />
           </div>
-          <div className={`${styles.info}`}>
-            <h3>{t.home.advantage.title3}</h3>
-            <p>
-            {t.home.advantage.description3}
-            </p>
-          </div>
-        </Link>
-      
-      
+        </div>
+        <div className={`${styles.info}`}>
+          <h3>{t.home.advantage.title3}</h3>
+          <p>{t.home.advantage.description3}</p>
+        </div>
+      </Link>
     </section>
   );
 };
